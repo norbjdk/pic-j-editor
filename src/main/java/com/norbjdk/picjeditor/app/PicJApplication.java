78261e58b29d19135791c6bcd77482ed01f0591c
ndominkiewicz <norbertd1410@gmail.com>
2026-04-13 23:09:36 +0200

    feat: add passing stage object for project service initialization

--- src/main/java/com/norbjdk/picjeditor/app/PicJApplication.java
+++ src/main/java/com/norbjdk/picjeditor/app/PicJApplication.java
@@ -1,5 +1,6 @@
 package com.norbjdk.picjeditor.app;
 
+import com.norbjdk.picjeditor.core.project.ProjectService;
 import com.norbjdk.picjeditor.ui.manager.ViewManager;
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -8,8 +9,12 @@ public class PicJApplication extends Application {
     private static final int MIN_WIDTH = 1200;
     private static final int MIN_HEIGHT = 650;
 
+    private Stage primaryStage;
+
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage stage) {
+        this.primaryStage = stage;
+
         final MainWindow mainWindow = new MainWindow();
 
         primaryStage.setTitle("Pic-J Editor");
@@ -24,5 +29,13 @@ public class PicJApplication extends Application {
     public void init() {
         final ViewManager viewManager = ViewManager.getInstance();
         System.out.println("Initialized View Manager");
+
+        final ProjectService projectService = ProjectService.getInstance();
+        projectService.init(primaryStage);
+        System.out.println("Initialized Project Service");
+    }
+
+    public Stage getPrimaryStage() {
+        return primaryStage;
     }
 }
