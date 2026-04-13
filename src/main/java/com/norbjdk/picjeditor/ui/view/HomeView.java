9d72f5ccebdae052b79340ad968aec53290cf828
ndominkiewicz <norbertd1410@gmail.com>
2026-04-13 19:40:16 +0200

    feat: add empty view classes for testing purposes

---
+++ src/main/java/com/norbjdk/picjeditor/ui/view/HomeView.java
@@ -0,0 +1,40 @@
+package com.norbjdk.picjeditor.ui.view;
+
+import com.norbjdk.picjeditor.ui.model.Presentable;
+import com.norbjdk.picjeditor.ui.model.Viewable;
+import javafx.scene.control.ScrollPane;
+
+public class HomeView extends ScrollPane implements Presentable, Viewable {
+
+    public HomeView() {}
+
+    @Override
+    public void initComponents() {
+
+    }
+
+    @Override
+    public void setupComponents() {
+
+    }
+
+    @Override
+    public void setupStyle() {
+
+    }
+
+    @Override
+    public void setupLayout() {
+
+    }
+
+    @Override
+    public void setupEventListeners() {
+
+    }
+
+    @Override
+    public void setupEventHandlers() {
+
+    }
+}
